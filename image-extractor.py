ec7227392dd378760c56e27372e755c0c9e4fe93
Shreyas Nargund <nargund.shreyas@gmail.com>
2018-07-03 23:27:41 +0530

    Util and image extractor

---
+++ image-extractor.py
@@ -0,0 +1,42 @@
+# This script can be used to convert input video file into number of snapshots(images) taken and regular configurable interval
+# author: shreyasn
+
+import argparse
+
+import cv2
+import pafy
+
+from util.utility import is_url
+
+
+def extract_images(video, image_path, interval=1):
+    count = 0
+    # check if video path string is url
+    print("in url")
+    if is_url(video):
+        video_pafy = pafy.new(video)
+        video = video_pafy.getbest().url
+
+    vid_cap = cv2.VideoCapture(video)
+    try:
+        success, image = vid_cap.read()
+        success = True
+        while success:
+            vid_cap.set(cv2.CAP_PROP_POS_MSEC, (count * (interval * 1000)))  # added this line
+            success, image = vid_cap.read()
+
+            if success:
+                print('New frame created: ', success)
+                cv2.imwrite(image_path + "\\frame%d.jpg" % count, image)  # save frame as JPEG file
+                count = count + 1
+
+    finally:
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("--video", help="path to video")
+    a.add_argument("--imagePath", help="path to images")
+    args = a.parse_args()
+    extract_images(args.video, args.imagePath, 1)
---
+++ utility.py
@@ -0,0 +1,26 @@
+# A general purpose utility script consisting number of useful methods.
+# author: shreyasn
+
+def is_url(path):
+    try:
+        result = urlparse(path)
+        return result.scheme and result.netloc and result.path
+    except:
+        return False
+
+
+def generate_colors(n, max_value=255):
+    colors = []
+    h = 0.1
+    s = 0.5
+    v = 0.95
+    for i in range(n):
+        h = 1 / (h + GOLDEN_RATIO)
+        colors.append([c * max_value for c in colorsys.hsv_to_rgb(h, s, v)])
+
+    return colors
+
+
+def find_class_by_name(name, modules):
+    modules = [getattr(module, name, None) for module in modules]
+    return next(a for a in modules if a)
